21cfa95defe4d024866d4c6dc3b6f2a51d484265
Nespokoyniy <r9888582703@gmail.com>
2025-12-22 14:00:03 +0300

    wip: добился рабочего gitlab и других контейнеров

--- run.py
+++ run.py
@@ -2,14 +2,26 @@
 import os
 import fire
 
+
 def docker_up():
     os.system("docker compose -f docker-compose.dev.yaml up")
-    
+
+
 def docker_build():
     os.system("docker compose -f docker-compose.dev.yaml build")
 
+
+def docker_all():
+    os.system(
+        "docker compose -f docker-compose.dev.yaml down && docker compose -f docker-compose.dev.yaml build && docker compose -f docker-compose.dev.yaml up"
+    )
+
+
 def docker_down():
     os.system("docker compose -f docker-compose.dev.yaml down")
-    
+
+
 if __name__ == "__main__":
-    fire.Fire({"up": docker_up, "build": docker_build, "down": docker_down})
+    fire.Fire(
+        {"up": docker_up, "build": docker_build, "down": docker_down, "all": docker_all}
+    )
