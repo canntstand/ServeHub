7aa2b5375de952df82dcd86b0c7c0280adb5a1b7
Roman Frolov <r9888582703@gmail.com>
2026-02-24 21:48:01 +0300

    wip: добавил .sh скрипт для быстрой настройки виртуального окружения

---
+++ scripts/venv-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python -m venv .venv
+
+source .venv/Scripts/activate
+
+echo "alias docdev='docker compose -f docker-compose.dev.yaml'" >> .venv/Scripts/activate
+echo "alias docprod='docker compose -f docker-compose.prod.yaml'" >> .venv/Scripts/activate
+
+pip install -r requirements.txt
+
+deactivate
