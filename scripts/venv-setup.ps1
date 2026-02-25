b0fb1dce8e0c341ddfe03e8a4b495ea24671f199
Roman Frolov <r9888582703@gmail.com>
2026-02-25 22:04:09 +0300

    wip: в процессе автоматизации файлов для создания окружения

--- scripts/venv-setup.ps1
+++ scripts/venv-setup.ps1
@@ -5,7 +5,8 @@ python -m venv .venv
 Add-Content -Path .venv\Scripts\Activate.ps1 -Value "function global:docdev {docker compose -f docker-compose.dev.yaml @args}"
 Add-Content -Path .venv\Scripts\Activate.ps1 -Value "function global:docprod {docker compose -f docker-compose.prod.yaml @args}"
 
-pip install -r requirements.txt
+pip install -r services\email-scheduler\requirements.txt
+pip install -r services\link-preview\requirements.txt
 python.exe -m pip install --upgrade pip
 
 deactivate
--- scripts/venv-setup.sh
+++ scripts/venv-setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ -d ".venv" ]; then
+    .venv/Scripts/activate
+    deactivate
+    rm -rf .venv
+fi
+
 python -m venv .venv
 
 source .venv/Scripts/activate
@@ -7,7 +13,16 @@ source .venv/Scripts/activate
 echo "alias docdev='docker compose -f docker-compose.dev.yaml'" >> .venv/Scripts/activate
 echo "alias docprod='docker compose -f docker-compose.prod.yaml'" >> .venv/Scripts/activate
 
-pip install -r requirements.txt
+cd services
+
+for service in */; do
+    cd "$service"
+    if [ -f "requirements.txt" ]; then
+        pip install -r "requirements.txt"
+    fi
+    cd ..
+done
+
 python.exe -m pip install --upgrade pip
 
 deactivate
