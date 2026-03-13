35721e76ed95186cdd1b9a9b313caa9b97db90ca
Roman Frolov <r9888582703@gmail.com>
2026-03-13 21:45:49 +0300

    fix: smtp в gitlab ce теперь работает нормально, к изменению пароля root добавилось изменение почты

--- INSTALLATION_GUIDE.md
+++ INSTALLATION_GUIDE.md
@@ -1,2 +1,7 @@
 # Гайд по установке и настройке проекта
+# Пока кратко
 
+1. Скрипты в scripts
+2. Git на хосте для синхронизации репозитория с GitLab CE
+    1. Создание репозитория в Gitlab CE, желательно с именем ServeHub
+    3. git remote set-url --add --push origin http://localhost:8081/root/ServeHub
--- scripts/root-gitlab-setup.sh
+++ scripts/root-gitlab-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# скрипт для установки пароля для root пользователя, ник пользователя - root
+# скрипт для установки пароля и почты для root пользователя, ник пользователя - root
 # перед использованием в контейнере лучше немного подождать до инициализации gitlab-ce
 
 set -e
@@ -9,4 +9,12 @@ echo "Смена пароля root..."
 
 echo -e "${ROOT_PASSWORD}\n${ROOT_PASSWORD}" | gitlab-rake "gitlab:password:reset[root]"
 
+echo "Смена почты root..."
+
+gitlab-rails runner "
+    user = User.find(1);
+    user.email = '${ROOT_EMAIL}';
+    user.skip_reconfirmation!;
+    user.save!
+"
 echo "Все готово!"
